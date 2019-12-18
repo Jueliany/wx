161cc94929d95872cf77995c668362082e9642ec
Jueliany <31821028+Jueliany@users.noreply.github.com>
2019-12-18 18:01:29 +0800

    none

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,76 +1,298 @@
-// pages/cart/cart.js
+
+
+var app = getApp();
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    editStatus:true,
-    carLeng: false,
-    cartList: [{
-      id: "233",
-      name:"【12期免息】Apple/苹果 iPhone 11 Pro MAX 2019新品 全网通手机 苹果11promax 拍照智能手机",
-      num:10,
-      imgListUrl:"https://file.quhappy.com/images/2018/11/09/12/19e0231930e44d72ad306200850b3d28.jpg",
-      choose2:false,
-      choose1:false
+    cartGoods: [{
+      "id": 1,
+      "user_id": 0,
+      "session_id": "1",
+      "goods_id": 1116011,
+      "goods_sn": "1116011",
+      "product_id": 167,
+      "goods_name": "蔓越莓曲奇 200克",
+      "market_price": 36,
+      "retail_price": 36,
+      "number": 3,
+      "goods_specifition_name_value": "",
+      "goods_specifition_ids": "",
+      "checked": 0,
+      "list_pic_url": "http://yanxuan.nosdn.127.net/767b370d07f3973500db54900bcbd2a7.png"
     }],
-    priceSum: 0
+    cartTotal: {
+      "goodsCount": 0,
+      "goodsAmount": 0.00,
+      "checkedGoodsCount": 0,
+      "checkedGoodsAmount": 0.00
+    },
+    isEditCart: false,
+    checkedAllStatus: true,
+    editCartList: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
+    // 页面渲染完成
 
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
-
+    // 页面显示
+    // this.getCartList();
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
   onHide: function () {
+    // 页面隐藏
 
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload: function () {
+    // 页面关闭
 
   },
+  getCartList: function () {
+    let that = this;
+    util.request(api.CartList).then(function (res) {
+      if (res.errno === 0) {
+        console.log(res.data);
+        that.setData({
+          cartGoods: res.data.cartList,
+          cartTotal: res.data.cartTotal
+        });
+      }
+      that.setData({
+        cartGoods: res.data.cartLists,
+        checkedAllStatus: that.isCheckedAll()
+      });
+    });
+  },
+  isCheckedAll: function () {
+    //判断购物车商品已全选
+    return this.data.cartGoods.every(function (element, index, array) {
+      if (element.checked == true) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  },
+  checkedItem: function (event) {
+    let itemIndex = event.target.dataset.itemIndex;
+    let that = this;
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    if (!this.data.isEditCart) {
+      // util.request(api.CartChecked, { productIds: that.data.cartGoods[itemIndex].product_id, isChecked: that.data.cartGoods[itemIndex].checked ? 0 : 1 }, 'POST').then(function (res) {
+      //   if (res.errno === 0) {
+      //     console.log(res.data);
+      //     that.setData({
+      //       cartGoods: res.data.cartList,
+      //       cartTotal: res.data.cartTotal
+      //     });
+      //   }
+      // });
+      console.log(that.data.cartGoods)
+      var cartLists = that.data.cartGoods;
+      cartLists[itemIndex].checked = that.data.cartGoods[itemIndex].checked ? 0 : 1;
+      that.setData({
+        cartGoods: cartLists,
+        checkedAllStatus: that.isCheckedAll()
+      });
+    } else {
+      //编辑状态
+      let tmpCartData = this.data.cartGoods.map(function (element, index, array) {
+        if (index == itemIndex) {
+          element.checked = !element.checked;
+        }
+
+        return element;
+      });
+
+      that.setData({
+        cartGoods: tmpCartData,
+        checkedAllStatus: that.isCheckedAll(),
+        'cartTotal.checkedGoodsCount': that.getCheckedGoodsCount()
+      });
+    }
+  },
+  getCheckedGoodsCount: function () {
+    let checkedGoodsCount = 0;
+    this.data.cartGoods.forEach(function (v) {
+      if (v.checked === true) {
+        checkedGoodsCount += v.number;
+      }
+    });
+    console.log(checkedGoodsCount);
+    return checkedGoodsCount;
+  },
+  checkedAll: function () {
+    let that = this;
+
+    if (!this.data.isEditCart) {
+      var productIds = this.data.cartGoods.map(function (v) {
+        return v.product_id;
+      });
+      util.request(api.CartChecked, { productIds: productIds.join(','), isChecked: that.isCheckedAll() ? 0 : 1 }, 'POST').then(function (res) {
+        if (res.errno === 0) {
+          console.log(res.data);
+          that.setData({
+            cartGoods: res.data.cartList,
+            cartTotal: res.data.cartTotal
+          });
+        }
+
+        that.setData({
+          checkedAllStatus: that.isCheckedAll()
+        });
+      });
+    } else {
+      //编辑状态
+      let checkedAllStatus = that.isCheckedAll();
+      let tmpCartData = this.data.cartGoods.map(function (v) {
+        v.checked = !checkedAllStatus;
+        return v;
+      });
+
+      that.setData({
+        cartGoods: tmpCartData,
+        checkedAllStatus: that.isCheckedAll(),
+        'cartTotal.checkedGoodsCount': that.getCheckedGoodsCount()
+      });
+    }
 
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+  editCart: function () {
+    var that = this;
+    if (this.data.isEditCart) {
+      // this.getCartList();
+      this.setData({
+        isEditCart: !this.data.isEditCart
+      });
+    } else {
+      //编辑状态
+      let tmpCartList = this.data.cartGoods.map(function (v) {
+        v.checked = false;
+        return v;
+      });
+      this.setData({
+        editCartList: this.data.cartGoods,
+        cartGoods: tmpCartList,
+        isEditCart: !this.data.isEditCart,
+        checkedAllStatus: that.isCheckedAll(),
+        'cartTotal.checkedGoodsCount': that.getCheckedGoodsCount()
+      });
+    }
 
   },
+  updateCart: function (productId, goodsId, number, id) {
+    let that = this;
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    util.request(api.CartUpdate, {
+      productId: productId,
+      goodsId: goodsId,
+      number: number,
+      id: id
+    }, 'POST').then(function (res) {
+      if (res.errno === 0) {
+        console.log(res.data);
+        that.setData({
+          //cartGoods: res.data.cartList,
+          //cartTotal: res.data.cartTotal
+        });
+      }
 
+      that.setData({
+        checkedAllStatus: that.isCheckedAll()
+      });
+    });
+
+  },
+  cutNumber: function (event) {
+
+    let itemIndex = event.target.dataset.itemIndex;
+    let cartItem = this.data.cartGoods[itemIndex];
+    let number = (cartItem.number - 1 > 1) ? cartItem.number - 1 : 1;
+    cartItem.number = number;
+    this.setData({
+      cartGoods: this.data.cartGoods
+    });
+    this.updateCart(cartItem.product_id, cartItem.goods_id, number, cartItem.id);
+  },
+  addNumber: function (event) {
+    let itemIndex = event.target.dataset.itemIndex;
+    let cartItem = this.data.cartGoods[itemIndex];
+    let number = cartItem.number + 1;
+    cartItem.number = number;
+    this.setData({
+      cartGoods: this.data.cartGoods
+    });
+    this.updateCart(cartItem.product_id, cartItem.goods_id, number, cartItem.id);
+
+  },
+  checkoutOrder: function () {
+    //获取已选择的商品
+    let that = this;
+
+    var checkedGoods = this.data.cartGoods.filter(function (element, index, array) {
+      if (element.checked == true) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    if (checkedGoods.length <= 0) {
+      return false;
+    }
+
+
+    wx.navigateTo({
+      url: '../shopping/checkout/checkout'
+    })
+  },
+  deleteCart: function () {
+    //获取已选择的商品
+    let that = this;
+
+    let productIds = this.data.cartGoods.filter(function (element, index, array) {
+      if (element.checked == true) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    if (productIds.length <= 0) {
+      return false;
+    }
+
+    productIds = productIds.map(function (element, index, array) {
+      if (element.checked == true) {
+        return element.product_id;
+      }
+    });
+
+
+    util.request(api.CartDelete, {
+      productIds: productIds.join(',')
+    }, 'POST').then(function (res) {
+      if (res.errno === 0) {
+        console.log(res.data);
+        let cartList = res.data.cartList.map(v => {
+          console.log(v);
+          v.checked = false;
+          return v;
+        });
+
+        that.setData({
+          cartGoods: cartList,
+          cartTotal: res.data.cartTotal
+        });
+      }
+
+      that.setData({
+        checkedAllStatus: that.isCheckedAll()
+      });
+    });
   }
 })
--- pages/main/main.js
+++ pages/main/main.js
@@ -7,7 +7,50 @@ Page({
   data: {
     goodsCount: 0,
     newGoods: [],
-    hotGoods: [],
+    hotGoods: [{
+      "id": 22,
+      "productTitle": "Apple iPhone 7 Plus (A1661) 128G 黑色 移动联通电信4G手机",
+      "productPicURL": "https://img14.360buyimg.com/n0/jfs/t1/18387/1/12727/161720/5c999df4E06720614/34ac75cc458efc46.jpg",
+      "price": "3499.00",
+      "isHot": 1
+    }, {
+      "id": 9,
+        "productTitle": "Apple iPad 平板电脑 2019年新款10.2英寸（128G WLAN版/iPadOS系统/Retina显示屏/MW792CH/A）金色",
+        "productPicURL": "https://img10.360buyimg.com/jdcms/s150x150_jfs/t1/45124/13/10457/96407/5d780a64Ef57cfbc1/61f65ac6aae3146b.jpg.webp",
+      "productLabelAry": ["大澳体验", "快捷换票", "即订即用"],
+      "price": "3299.00",
+      "isHot": 0
+    }, {
+      "id": 10,
+        "productTitle": "索尼（SONY）Alpha 7R III 机身 全画幅微单数码相机（约4240万有效像素 5轴防抖 a7RM3/a7r3/a73）",
+        "productPicURL": "https://img14.360buyimg.com/n0/jfs/t19249/206/1908637936/308650/6fc8ef07/5add8fb8N72996f51.jpg",
+      "price": "14999.00",
+      "isHot": 0
+    }, {
+      "id": 11,
+        "productTitle": "佳能（Canon）EOS 200D II 200D2 迷你单反相机 数码相机（EF-S18-55mm f/4-5.6 IS STM）黑色 Vlog相机视频",
+        "productPicURL": "https://img13.360buyimg.com/n1/s450x450_jfs/t1/35828/26/5078/324781/5cbea100Eb22bb637/4d2d5e3bcd86fba4.jpg",
+      "price": "12234.00",
+      "isHot": 0
+    }, {
+      "id": 12,
+        "productTitle": "联想ThinkPad 翼480（4VCD）英特尔酷睿i5 14英寸轻薄笔记本电脑(i5-8250U 8G 128GSSD+1T 2G独显 FHD)冰原银",
+        "productPicURL": "https://img14.360buyimg.com/n0/jfs/t1/42514/11/1912/262553/5cc7e219Ee6a3fb7b/f7b38dfa82781d9b.jpg",
+      "price": "4999.00",
+      "isHot": 0
+    }, {
+      "id": 23,
+        "productTitle": "荣耀V20 游戏手机 麒麟980芯片 魅眼全视屏 4800万深感相机 8GB+256GB 幻影红 移动联通电信4G全面屏手机",
+        "productPicURL": "https://img14.360buyimg.com/n0/jfs/t1/18538/6/15338/313665/5cb0a8e0Ee9117074/878a30dad2f172f8.jpg",
+      "price": "1999.00",
+      "isHot": 0
+    }, {
+      "id": 25,
+        "productTitle": "华为 HUAWEI P30 Pro 超感光徕卡四摄10倍混合变焦麒麟980芯片屏内指纹 8GB+128GB极光色全网通版双4G手机",
+        "productPicURL": "https://img14.360buyimg.com/n0/jfs/t1/19261/13/12605/324178/5c98c7bcE63f668de/ca2762256ec6f931.jpg",
+      "price": "4480.00",
+      "isHot": 0
+    }],
     topics: [],
     brands: [],
     floorGoods: [],
